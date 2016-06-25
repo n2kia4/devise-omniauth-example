8ffa75db084b018d3a02b26a9f015beba598ab3a
Natsuki Tanaka <natsukia3@gmail.com>
2016-06-25 17:10:56 +0900

    Set of basic gems

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,15 @@ Bundler.require(*Rails.groups)
 
 module DeviseOmniauthExample
   class Application < Rails::Application
+    config.generators do |g|
+      g.helper false
+      g.stylesheets false
+      g.javascripts false
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
