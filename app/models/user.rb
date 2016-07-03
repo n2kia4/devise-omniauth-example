83088f87d7ae06fb1bb41d6dd24df124cae2ddd9
Natsuki Tanaka <natsukia3@gmail.com>
2016-07-03 16:30:46 +0900

    Add identity model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable
+
+  has_many :identities, dependent: :destroy
 
   validates :name, presence: true
 end
