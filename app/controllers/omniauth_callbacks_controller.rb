6802a349a7564ab5d3f6ce00340975a43ef0989d
Natsuki Tanaka <natsukia3@gmail.com>
2016-07-03 21:56:45 +0900

    Add authentication in omniauth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,2 +1,17 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+  def all
+    user = User.from_omniauth(env["omniauth.auth"], current_user)
+    if user.persisted?
+      flash[:notice] = "Signed in!"
+      sign_in_and_redirect(user)
+    else
+      session["devise.user_attributes"] = user.attributes
+      redirect_to new_user_registration_url
+    end
+  end
+
+  alias_method :twitter, :all
+  alias_method :github, :all
+  alias_method :google, :all
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,44 @@ class User < ActiveRecord::Base
   has_many :identities, dependent: :destroy
 
   validates :name, presence: true
+
+  def self.new_with_session(params,session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"],without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
+
+  def self.from_omniauth(auth, current_user, signed_in_resource = nil)
+    identity = Identity.find_for_oauth(auth)
+    user = signed_in_resource ? signed_in_resource : identity.user
+    if identity.user.nil?
+      user = current_user || User.where('email = ?', auth["info"]["email"]).first
+      if user.nil?
+        user = User.new
+        user.name = auth.info.name
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,10]
+        if auth.provider == 'twitter'
+          user.email = User.dummy_email(auth)
+        end
+        user.save
+      end
+    end
+    if identity.user != user
+      identity.user = user
+      identity.save
+    end
+    user
+  end
+
+  private
+
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
 end
